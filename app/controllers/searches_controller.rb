2d63b546422bb15f2b18424f69288252f9170b3f
DJ Martin <umdjm@hotmail.com>
2013-12-06 17:03:45 -0500

    asset issue

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -46,7 +46,7 @@ class SearchesController < ApplicationController
 
     respond_to do |format|
       if @search.save
-        format.html { redirect_to @search, notice: 'Search was successful.' }
+        format.html { redirect_to searches_url, notice: 'Search was successful.' }
         format.json { render json: @search, status: :created, location: @search }
       else
         format.html { render action: "new" }
@@ -62,7 +62,7 @@ class SearchesController < ApplicationController
 
     respond_to do |format|
       if @search.update_attributes(params[:search])
-        format.html { redirect_to @search, notice: 'Search was successfully updated.' }
+        format.html { redirect_to searches_url, notice: 'Search was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
@@ -11,6 +10,7 @@ end
 
 module DealerLocator
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
